c4985049e15da0d1aac9832d4c208ffc82a3b612
zhmhbest <zhmhbest@gmail.com>
2021-02-08 14:19:31 +0800

    update

--- docs/spark/src/main/scala/org/example/HelloSpark.scala
+++ docs/spark/src/main/scala/org/example/HelloSpark.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.{DataFrameReader, SparkSession}
 object HelloSpark {
 
   def main(args: Array[String]): Unit = {
-    val RESOURCE_PATH = this.getClass.getClassLoader.getResource("./").getPath
+//    val RESOURCE_PATH = this.getClass.getClassLoader.getResource("./").getPath
 
     val spark = SparkSession.builder
       .config("spark.driver.host", "localhost")
@@ -15,8 +15,8 @@ object HelloSpark {
     spark.sparkContext.setLogLevel("ERROR")
 
     val dfReader: DataFrameReader = spark.read.format("csv")
-
-    val df = dfReader.csv(RESOURCE_PATH + "csv/Order.csv")
+    val csvFile = this.getClass.getClassLoader.getResource("./csv/Order.csv").getFile
+    val df = dfReader.csv(csvFile)
     df.foreach(r => {
       println(r)
     })
